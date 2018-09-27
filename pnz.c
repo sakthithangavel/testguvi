77c975d5c5ca5bb50d5d9e3ac887a986304d4022
sakthithangavel <sakthisiva933@gmail.com>
2018-09-27 19:57:32 +0530

    Create pnz.c

---
+++ pnz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n<0)
+{
+  printf("Negative");
+}
+else 
+{
+   if(n==0)
+    printf("Zero");
+   else
+    printf("Positive");
+}
+return 0;
+}
