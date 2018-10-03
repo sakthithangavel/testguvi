2af2514e470959e843e04589ce8a8b8ee4fcf4f0
sakthithangavel <sakthisiva933@gmail.com>
2018-10-03 18:09:07 +0530

    Create duplicate.java

---
+++ test/src/test/duplicate.java
@@ -0,0 +1,41 @@
+package test;
+import java.util.Scanner;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.LinkedHashMap;
+public class duplicate {
+   static Scanner sc;
+	public static void main(String[] args) 
+	{
+		sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		LinkedHashSet<Integer> se=new LinkedHashSet<Integer>();
+		LinkedHashMap<Integer,Integer> m=new LinkedHashMap<Integer,Integer>();
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+			se.add(arr[i]);
+		}
+		for(Integer i:se)
+		{
+			int count=1;
+			m.put(i, count);
+			for(int j:arr)
+			{
+				if(i==j)
+				{
+				   m.put(i, count++);	
+				}
+			}
+		}
+		for(Map.Entry<Integer,Integer> e:m.entrySet())
+		{
+			if(e.getValue()>1)
+			{
+				System.out.print(e.getKey()+" ");
+			}
+		}
+	}
+
+}
