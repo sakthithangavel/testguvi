bb33c16767217c8e72787babe2081944145cb61a
sakthithangavel <sakthisiva933@gmail.com>
2018-10-15 19:16:48 +0530

    Create axby.java

---
+++ test/src/test/axby.java
@@ -0,0 +1,13 @@
+package test;
+import java.util.Scanner;
+public class axby {
+   static Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int x=sc.nextInt();
+    System.out.print((a*x)+b);
+	}
+
+}
